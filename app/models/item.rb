d224b116d21df006e87c5b4982373e28b14b6330
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-01-14 18:28:02 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+  has_one :purchase
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :area
+  belongs_to :day
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :state_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :days_id, numericality: { other_than: 1 }
+
+
 end
