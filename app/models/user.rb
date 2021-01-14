4c0aa805f3581926e88ca241d5cf3b177ebe839c
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-01-14 12:16:10 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_family_name, presence: true
-  validates :kana_first_name, presence: true
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'Include both letters and numbers' } do
+    validates :password
+  end 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with:/\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do
+    validates :kana_family_name
+    validates :kana_first_name
+  end
   validates :birth_day, presence: true
 
   has_many :items
