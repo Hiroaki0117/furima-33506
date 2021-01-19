5914096afa7ed3e30ced26747b36084fb2c169d4
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-01-20 08:48:30 +0900

    バリデーション、アソシエーション追記

--- app/models/item_purcchase.rb
+++ app/models/item_purcchase.rb
@@ -1,8 +1,9 @@
 class ItemPurchase
   include ActiveModel::Model
-  attr_accessor
+  attr_accessor :token, :post_code, :city, :street_number, :buildings, :phone_number
   
   with_options presence: true do
+    validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :city
     validates :street_number
