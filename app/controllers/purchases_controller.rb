00627e288b4e67d4a1375cd2a6233e93dfd4fa1f
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-01-20 21:59:44 +0900

    payjp 導入

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,6 +7,7 @@ class PurchasesController < ApplicationController
   def create
     @item_purchase = ItemPurchase.new(purchase_params)
     if @item_purchase.valid?
+      pay_item
       @item_purchase.save
       redirect_to root_path
     else
@@ -17,10 +18,19 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:item_purchase).permit(:post_code, :area_id, :city, :street_number, :buildings, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_purchase).permit(:post_code, :area_id, :city, :street_number, :buildings, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_bb63b2b0c1427452101cbb48"
+    Payjp::Charge.create(
+      amount: set_item[:price],
+      card: purchase_params[:token],
+      currency: "jpy"
+    )
   end
 end
--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,16 +1,17 @@
 class ItemPurchase
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :city, :street_number, :buildings, :phone_number, :user_id, :item_id
+  attr_accessor :token, :post_code, :area_id, :city, :street_number, :buildings, :phone_number, :user_id, :item_id
   
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :city
     validates :street_number
     validates :phone_number, format: { with: /\A0[5789]0[-]?\d{4}[-]?\d{4}\z/, message: "Input only number" }
+    validates :token
   end
   validates :area_id, numericality: { other_than: 1 }, presence: true
   def save
-    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    purchase = Purchase.create( user_id: user_id, item_id: item_id)
     Address.create(post_code: post_code, area_id: area_id, city: city, street_number: street_number, buildings: buildings, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
