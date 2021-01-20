ce6235d80d503ba9c4b5de2de355c307447d3c90
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-01-20 16:00:00 +0900

    item_purchaseモデルの修正2

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,9 +1,8 @@
 class ItemPurchase
   include ActiveModel::Model
-  attr_accessor :post_code, :area_id, :city, :street_number, :buildings, :phone_number
+  attr_accessor :post_code, :area_id, :city, :street_number, :buildings, :phone_number, :user_id, :item_id
   
   with_options presence: true do
-    validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :city
     validates :street_number
@@ -11,7 +10,7 @@ class ItemPurchase
   end
   validates :area_id, numericality: { other_than: 1 }, presence: true
   def save
-    Address.create(post_code: post_code, area_id: area_id, city: city, street_number: street_number, buildings: buildings, phone_number: phone_number, user_id: user.id)
-    Purchase.create(user_id: user.id, item_id: item.id)
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, area_id: area_id, city: city, street_number: street_number, buildings: buildings, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
