c7058a33296a3c788ee08bfa78196a260643f9bb
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-01-20 12:44:06 +0900

    purchaseコントローラー記述1

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,26 @@
 class PurchasesController < ApplicationController
+  before_action :set_item
   def index
+    @item_purchase = ItemPurchase.new
+  end
+
+  def create
+    @item_purchase = ItemPurchase.new(purchase_params)
+    if @item_purchase.valid?
+      @item_purchase.save
+      redirect_to action: :index
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:post_code, :area_id, :city, :street_number, :buildings, :phone_number)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
