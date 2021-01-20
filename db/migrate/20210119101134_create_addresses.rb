f61bd796e6a4c5e83e7061ce49a75775aa85a935
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-01-20 12:52:23 +0900

    adressのマイグレーションファイル　修正

--- db/migrate/20210119101134_create_addresses.rb
+++ db/migrate/20210119101134_create_addresses.rb
@@ -7,7 +7,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string     :street_number, null: false
       t.string     :buildings
       t.string     :phone_number,  null: false
-      t.references :user,          null: false, foreign_key: true
+      t.references :purchase,      null: false, foreign_key: true
       t.timestamps
     end
   end
