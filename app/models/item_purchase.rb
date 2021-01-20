18bc46b31836c29c7f67d1227334ed5e5de3f22d
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-01-20 11:40:10 +0900

    item_purchaseモデルの修正

--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,6 +1,6 @@
 class ItemPurchase
   include ActiveModel::Model
-  attr_accessor :token, :post_code, :city, :street_number, :buildings, :phone_number
+  attr_accessor :post_code, :area_id, :city, :street_number, :buildings, :phone_number
   
   with_options presence: true do
     validates :token
@@ -11,5 +11,7 @@ class ItemPurchase
   end
   validates :area_id, numericality: { other_than: 1 }, presence: true
   def save
+    Address.create(post_code: post_code, area_id: area_id, city: city, street_number: street_number, buildings: buildings, phone_number: phone_number, user_id: user.id)
+    Purchase.create(user_id: user.id, item_id: item.id)
   end
 end
